1e5ec3e282fb5849d4d37bf6dc23b199e460d59b
supalerk <supalerk_ar66@live.rmutl.ac.th>
2024-10-10 22:23:02 +0700

    1

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,42 +1,39 @@
+const passport = require('passport');
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
+
 module.exports = {
-    registerView: (req, res) => { 
-      res.render('register');
-    },
-  
-    loginView: (req, res) => {
-      res.render('login');
-    },
-  
-    registerUser: async (req, res) => {
-        const { name, email, password } = req.body;
-        if(!name || !email || !password) {
-          return res.render('register', { error: 'Please fill all fields' });
-        }
-      
-        if(await User.findOne({where: {email}})) {
-          return res.render('register', { error: 'A user account already exists with this email' });
-        }
-      
-        await User.create({name, email, password: bcrypt.hashSync(password, 8)});
-      
-        res.redirect('login?registrationdone');
-      },
-  
-    loginUser: (req, res) => {
-      // TODO: complete
-      passport.authenticate('local', {
-        successRedirect: '/?loginsuccess',
-        failureRedirect: '/login?error'
-      })(req, res);
-    },
-  
-    logoutUser: (req, res) => {
-      // TODO: complete
-      res.redirect('login');
+  registerView: (req, res) => {
+    res.render('register');
+  },
+
+  loginView: (req, res) => {
+    res.render('login');
+  },
+
+  registerUser: async (req, res) => {
+    const { name, email, password } = req.body;
+    if(!name || !email || !password) {
+      return res.render('register', { error: 'Please fill all fields' });
     }
+
+    if(await User.findOne({where: {email}})) {
+      return res.render('register', { error: 'A user account already exists with this email' });
+    }
+
+    await User.create({name, email, password: bcrypt.hashSync(password, 8)});
+
+    res.redirect('login?registrationdone');
+  },
+
+  loginUser: (req, res) => {
+    passport.authenticate('local', {
+      successRedirect: '/?loginsuccess',
+      failureRedirect: '/login?error'
+    })(req, res);
+  },
+
+  logoutUser: (req, res) => {
+    req.logout(() => res.redirect('/login?loggedout'));
   }
-//ที่นี่ ฟังก์ชัน registerViewและloginViewตัวจัดการจะแสดงทั้ง มุมมอง และฟังก์ชันตามลำดับ 
-//เราจะใช้ ฟังก์ชัน และเพื่อลงทะเบียนและรับรองผู้ใช้หลังจากใช้งาน
-//โมเดลแล้วregister.puglogin.pugregisterUserloginUser
+}
